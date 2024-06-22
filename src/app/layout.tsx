08fd16d80de279042e3e3bc146b100e97f05dfe5
abrar997 <abraralrawi997.com>
2024-06-23 02:21:27 +0300

    update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Abrar Muthana Rakea",
   description: "Frontend web developer",
+  icons: "assets/me.jpg",
 };
 
 export default function RootLayout({
